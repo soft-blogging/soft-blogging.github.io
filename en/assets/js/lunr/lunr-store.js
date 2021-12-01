a2d314d307c127ff62761748f8799fe1e9653038
dev <user@users.noreply.github.com>
2021-12-01 20:45:02 +0300

    up

---
+++ en/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Об Авторе - Josh Rager",
+        "excerpt":"                         **   ","categories": ["profiles"],
+        "tags": [],
+        "url": "/ru/profiles/josh-rager/",
+        "teaser": "/assets/images/thumbs/500x300.webp"
+      },]
