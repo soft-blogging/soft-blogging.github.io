823a1fe222ea079409994e5562b4440a577aec7f
dev <user@users.noreply.github.com>
2021-12-01 21:28:55 +0300

    9

--- en/assets/js/lunr/lunr-store.js
+++ en/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "About the Author - Josh Rager",
-        "excerpt":"                         **   ","categories": ["profiles"],
+        "excerpt":"         Josh Rager - An expert in economics, finance, auditing, accounting and information technology.          In recent years he has been very passionate about blockchain technology.   Josh Rager - An expert in economics, finance, auditing, accounting and information technology, in recent years he has been very passionate about blockchain technology.   ","categories": ["profiles"],
         "tags": [],
         "url": "/ru/profiles/josh-rager/",
         "teaser": "/assets/images/thumbs/500x300.webp"
