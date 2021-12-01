13bf1ee762f1064d10b0b5fcb1f61f6945c84302
dev <user@users.noreply.github.com>
2021-12-01 22:52:14 +0300

    354

--- en/assets/js/lunr/lunr-store.js
+++ en/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "About the Author - Josh Rager",
-        "excerpt":"         Josh Rager - An expert in economics, finance, auditing, accounting and information technology.          In recent years he has been very passionate about blockchain technology.   Josh Rager - An expert in economics, finance, auditing, accounting and information technology, in recent years he has been very passionate about blockchain technology.   ","categories": ["profiles"],
+        "excerpt":"         Josh Rager - An expert in economics, finance, auditing, accounting and information technology.          In recent years he has been very passionate about blockchain technology.   Josh Rager   Josh Rager is an entrepreneur and developer, founder of the BlockRoots blockchain service and several other blockchain startups, as well as the winner of three American hackathons.   Entrepreneur, crypto enthusiast and blockchain evangelist with extensive consulting experience for the largest organizations in Russia and the United States.  ","categories": ["profiles"],
         "tags": [],
         "url": "/ru/profiles/josh-rager/",
         "teaser": "/assets/images/thumbs/500x300.webp"
